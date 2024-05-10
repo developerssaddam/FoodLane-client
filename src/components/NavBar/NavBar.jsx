5e9879d89462cc68216e1f3379417318a92c3c78
developersaddam <developerssaddam@gmail.com>
2024-05-10 23:14:28 +0600

    Added navbar and footer sections

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,102 @@
-
+import { Link, NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
 
-export default NavBar
+      <li>
+        <NavLink to="/allfoods">AllFoods</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/gallery">Gallery</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-[#4D4C7D]">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            id="menu"
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {links}
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost text-xl text-white font_lobster">
+          FoodLane
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul id="menu" className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end font_lato">
+        {/* <div className="dropdown dropdown-end mr-2">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+        <Link to='/' className="btn btn-sm">
+          Logout
+        </Link> */}
+
+        <Link to="/login" className="btn btn-sm">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
+import "./Layout.css";
 
 const Layout = () => {
   return (
     <div>
       <NavBar />
-      <Outlet />
+      <div className="outlet">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
