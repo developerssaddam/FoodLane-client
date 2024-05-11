da6328d5cb82ce79da6607850393539850d4229c
developersaddam <developerssaddam@gmail.com>
2024-05-11 14:23:42 +0600

    Added Login and Logout btn with conditional navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -82,7 +82,11 @@ const NavBar = () => {
                 <div className="w-10 rounded-full">
                   <img
                     alt="Tailwind CSS Navbar component"
-                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    src={
+                      user?.photoURL
+                        ? user?.photoURL
+                        : "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    }
                   />
                 </div>
               </div>
